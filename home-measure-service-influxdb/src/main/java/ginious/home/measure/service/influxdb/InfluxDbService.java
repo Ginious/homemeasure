215f05d25299798c64f588bd7c22c4a63b2ad123
christian <christian@192.168.2.28>
2020-06-14 17:19:59 +0200

    added logging and fixed activation.

--- home-measure-service-influxdb/src/main/java/ginious/home/measure/service/influxdb/InfluxDbService.java
+++ home-measure-service-influxdb/src/main/java/ginious/home/measure/service/influxdb/InfluxDbService.java
@@ -19,99 +19,97 @@ import ginious.home.measure.model.Measure;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Service for writing measures into an InfluxDB.
- * One InfluxDB measure will be created per device containing the HMServer measure id and its value.
+ * Service for writing measures into an InfluxDB. One InfluxDB measure will be
+ * created per device containing the HMServer measure id and its value.
  */
 @Slf4j
 @Service
-@ConditionalOnProperty(name = "enabled", havingValue = "true")
+@ConditionalOnProperty(prefix = InfluxDbServiceConfig.CONFIG_PREFIX, name = "enabled", havingValue = "true")
 public final class InfluxDbService extends AbstractService {
 
-  @Autowired
-  private InfluxDbServiceConfig config;
+	@Autowired
+	private InfluxDbServiceConfig config;
 
-  private static final String POINT_FIELD_VALUE = "value";
-  private static final String POINT_TAG_MEASURE_ID = "measure_id";
-  private static final String QUERY_CREATE_DATABASE = "CREATE DATABASE %s";
-  private static final String QUERY_SHOW_DATABASES = "SHOW DATABASES";
+	private static final String POINT_FIELD_VALUE = "value";
+	private static final String POINT_TAG_MEASURE_ID = "measure_id";
+	private static final String QUERY_CREATE_DATABASE = "CREATE DATABASE %s";
+	private static final String QUERY_SHOW_DATABASES = "SHOW DATABASES";
 
-  private InfluxDB influxDB;
+	private InfluxDB influxDB;
 
-  /**
-   * Default constructor.
-   * 
-   * @param inProperties
-   *          The overall application properties.
-   */
-  public InfluxDbService() {
-    super();
-  }
+	/**
+	 * Default constructor.
+	 * 
+	 * @param inProperties
+	 *            The overall application properties.
+	 */
+	public InfluxDbService() {
+		super();
+	}
 
-  @Override
-  protected void measureChangedCustom(Measure inChangedMeasure) {
+	@Override
+	protected void measureChangedCustom(Measure inChangedMeasure) {
 
-    Number valueAsNumber = null;
-    try {
-      valueAsNumber = NumberUtils.createNumber(inChangedMeasure.getValue().trim());
-    }
-    catch (NumberFormatException e) {
-      // ignore and do not write measure
-    } // catch
+		Number valueAsNumber = null;
+		try {
+			valueAsNumber = NumberUtils.createNumber(inChangedMeasure.getValue().trim());
+		} catch (NumberFormatException e) {
+			// ignore and do not write measure
+		} // catch
 
-    if (valueAsNumber != null) {
+		if (valueAsNumber != null) {
 
-      getInfluxDB().write(Point//
-          .measurement(inChangedMeasure.getDeviceId()) //
-          .tag(POINT_TAG_MEASURE_ID, inChangedMeasure.getId()) //
-          .addField(POINT_FIELD_VALUE, valueAsNumber).build());
+			getInfluxDB().write(Point//
+					.measurement(inChangedMeasure.getDeviceId()) //
+					.tag(POINT_TAG_MEASURE_ID, inChangedMeasure.getId()) //
+					.addField(POINT_FIELD_VALUE, valueAsNumber).build());
 
-      log.info("Persisted measure [{}.{}={}]", inChangedMeasure.getDeviceId(),
-          inChangedMeasure.getId(), inChangedMeasure.getValue());
-      
-    } // if
-  }
+			log.debug("Persisted measure [{}.{}={}]", inChangedMeasure.getDeviceId(), inChangedMeasure.getId(),
+					inChangedMeasure.getValue());
 
-  /**
-   * Gets a connection to the InfluxDB.
-   * 
-   * @return The connection to the InfluxDB.
-   */
-  private InfluxDB getInfluxDB() {
+		} // if
+	}
 
-    if (influxDB == null) {
+	/**
+	 * Gets a connection to the InfluxDB.
+	 * 
+	 * @return The connection to the InfluxDB.
+	 */
+	private InfluxDB getInfluxDB() {
 
-      log.info("Connecting InfluxDB using [{}/{}] to [{}]",
-          config.getUser() != null ? config.getUser() : "<anonymous>",
-              config.getPassword() != null ? "*******" : "none", config.getUrl());
-      
-      influxDB = InfluxDBFactory.connect(config.getUrl(), config.getUser(), config.getPassword());
+		if (influxDB == null) {
 
-      // gather existing database names
-      final List<String> existingDbs = new ArrayList<String>();
-      QueryResult dbList = influxDB.query(new Query(QUERY_SHOW_DATABASES));
-      dbList.getResults().forEach( //
-          result -> result.getSeries().forEach( //
-              serie -> serie.getValues().forEach( //
-                  value -> value.forEach(//
-                      dbname -> existingDbs.add(String.valueOf(dbname))))));
+			log.info("Connecting InfluxDB as [{}/{}] to [{}]",
+					config.getUser() != null ? config.getUser() : "<anonymous>",
+					config.getPassword() != null ? "*******" : "none", config.getUrl());
 
-      // create database if not yet existing
-      if (!existingDbs.contains(config.getDbname())) {
-        influxDB.query(new Query(String.format(QUERY_CREATE_DATABASE, config.getDbname())));
-        
-        log.info("Created database [{}]");
-      } // if
+			influxDB = InfluxDBFactory.connect(config.getUrl(), config.getUser(), config.getPassword());
 
-      influxDB.setDatabase(config.getDbname());
-      influxDB.enableBatch(BatchOptions.DEFAULTS.flushDuration(config.getFlush_duration()));
-      influxDB.enableGzip();
-    } // if
+			// gather existing database names
+			final List<String> existingDbs = new ArrayList<String>();
+			QueryResult dbList = influxDB.query(new Query(QUERY_SHOW_DATABASES));
+			dbList.getResults().forEach( //
+					result -> result.getSeries().forEach( //
+							serie -> serie.getValues().forEach( //
+									value -> value.forEach(//
+											dbname -> existingDbs.add(String.valueOf(dbname))))));
 
-    return influxDB;
-  }
+			// create database if not yet existing
+			if (!existingDbs.contains(config.getDbname())) {
+				influxDB.query(new Query(String.format(QUERY_CREATE_DATABASE, config.getDbname())));
+				log.info("Created database [{}]");
+			} // if
 
-  @Override
-  protected void initCustom() {
-    initIncludedMeasures(config.getIncluded_measures());
-  }
+			influxDB.setDatabase(config.getDbname());
+			influxDB.enableBatch(BatchOptions.DEFAULTS.flushDuration(config.getFlush_duration()));
+			influxDB.enableGzip();
+		} // if
+
+		return influxDB;
+	}
+
+	@Override
+	protected void initCustom() {
+		initIncludedMeasures(config.getIncluded_measures());
+	}
 }
